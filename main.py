ab9d1d791e07ae06c7d43fcc94f396a886df1f66
Dansito <Dansito@dev>
2021-07-20 19:28:56 -0400

    Fix 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import random
 from discord import message
 from discord.ext import commands
-from discord.ext.commands.core import command
+from discord.ext.commands.core import check, command
 from mtranslate import translate
 
 
@@ -17,15 +17,35 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Test'))
     msg = "Transitor online"
     print(msg)
+
+@client.command()
+async def en_es(ctx):
+    await ctx.send('Now, Enter the word to translate ')
     
-@client.command(name='en_es', help='Translate en-es')
-async def en_es(ctx, *, message=''):
-    text = ctx.message 
-    translate_en_es = translate(text, 'es', 'auto')
-    await ctx.send(translate_en_es)
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+    msg = await client.wait_for("message", check=check)
+    translate_en_es = translate(msg, 'es', 'auto')
+    await ctx.send (translate_en_es)
     
+    
+@client.command()
+async def command(ctx):
+    computer = random.randint(1, 10)
+    await ctx.send('Guess my number')
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel and int(msg.content) in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    msg = await client.wait_for("message", check=check)
+
+    if int(msg.content) == computer:
+        await ctx.send("Correct")
+    else:
+        await ctx.send(f"Nope it was {computer}")
     
 
+    
     
  #Token
 token = os.getenv("DISCORD_TOKEN") 
