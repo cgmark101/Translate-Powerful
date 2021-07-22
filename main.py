3aec9bba74eb6334aad76f176426073d0ffadc37
cgmark101 <cgmark101@gmail.com>
2021-07-21 22:43:40 -0400

    magic

--- main.py
+++ main.py
@@ -11,19 +11,17 @@ from typing import Optional
 #Json-Configs
 with open ("./config.json") as configjsonFile:
     configData = json.load(configjsonFile)
-    LANGUAGE = configData["LANGUAGES"]
-    PREFIX = configData["PREFIX_DEFAULT"]
-    PRASE = configData["LANGUAGES_PHRASE"]
 
 
 #Prefix
-client = commands.Bot(command_prefix=PREFIX, description="Auto-translate Bot")
+client = commands.Bot(command_prefix=configData["PREFIX_DEFAULT"], description="Auto-translate Bot")
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Test'))
     msg = "Transitor online"
     print(msg)
+    print(os.getenv("LANG"))
       
 
 @client.command()
@@ -36,27 +34,26 @@ async def lang(ctx):
     msg = await client.wait_for("message", check=check)
 
     if msg.content in configData["LANGUAGES"]:
-        LANGUAGE = configData["LANGUAGES"][msg.content]
-        await ctx.send (LANGUAGE)
-        print(LANGUAGE)
+        os.environ["LANG"] = msg.content
+        LANG = configData["LANGUAGES"][msg.content]
+        await ctx.send (LANG)
+        print(LANG)
     else:
         await ctx.send('The selected language does not exist, enter again, you can check in the help command to know the available languages')
         
 
 
-
-
-
 # Universal Translator
 @client.command()
 async def ts(ctx):
-    await ctx.send('frase')
+    LANG = os.getenv("LANG")
+    await ctx.send(configData["LANGUAGES_PHRASE"][LANG])
     
     def check(msg):
         return msg.author == ctx.author and msg.channel == ctx.channel
 
     msg = await client.wait_for("message", check=check)
-    translator = translate(msg.content, LANGUAGE, 'auto')
+    translator = translate(msg.content, LANG, 'auto')
     print(translator)
     await ctx.send (translator)
 
